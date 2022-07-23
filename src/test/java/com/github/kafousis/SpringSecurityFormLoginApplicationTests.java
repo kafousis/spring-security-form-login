cac20229db12e9101c1ed72f021fdb1f9d09c312
Giannis Kafousis <g.kafousis@gmail.com>
2022-07-23 20:44:05 +0300

    project init

---
+++ src/test/java/com/github/kafousis/SpringSecurityFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kafousis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFormLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
